dcd182ff6ffe14757b9f7742c450a0c9a04fbfc0
JLramosSoares <83511122+JLramosSoares@users.noreply.github.com>
2021-09-10 21:06:49 -0300

    Refatoração de código

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,22 +2,18 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
-	pygame.init()
-	ai_settings = Settings()
-	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
-	pygame.display.set_caption("Alien Invasion")
-	ship = Ship(screen)
-	bg_color = ai_settings.bg_color
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    ship = Ship(screen)
+    bg_color = ai_settings.bg_color
 
-	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.quit:
-				sys.exit()
-			
-			screen.fill(bg_color)
-			ship.blitme()
-			pygame.display.flip()
+    while True:
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
