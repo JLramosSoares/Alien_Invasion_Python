bfe3253a58c3b7f7149c9c8a239567db9072fe87
JLramosSoares <83511122+JLramosSoares@users.noreply.github.com>
2021-09-14 11:30:41 -0300

    Resolvendo pequeno erro

--- game_function.py
+++ game_function.py
@@ -20,7 +20,7 @@ def check_keyup_events(event, ship):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
-        ship.moving_right = False
+        ship.moving_left = False
 
 def update_screen(ai_settings, screen, ship):
     screen.fill(ai_settings.bg_color)
